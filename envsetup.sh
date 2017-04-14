378c763baa816190090e285b6485edb74771a7a0
Luca Belluccini <lucabelluccini@gmail.com>
2017-04-14 23:25:31 +0200

    Add missing commands

--- envsetup.sh
+++ envsetup.sh
@@ -4,6 +4,8 @@ NVM_VERSION=0.33.1
 NODE_VERSION=6.1.0
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install $NODE_VERSION
 nvm use --delete-prefix v$NODE_VERSION
 
